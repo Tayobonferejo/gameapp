3f99e5c3e003624ba81f69e94177d950d141b299
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-02-05 20:36:19 +0100

    Done fixed DOM duplicate of <i> icon

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 
-
 const main = document.getElementById("main")
 
 let change = true
 
 main.addEventListener("click", (event) => {
     const cross = event.target;
-    const crust = event.currentTarget;
 
 
 
+     // make sure only cells are clickable
+    if (cross.parentElement !== main) return;
 
 
-        if(change)
-        {
-            
-            const gameIcon = document.createElement("i");
-            gameIcon.classList.add("fa-solid", "fa-x");
-            cross.appendChild(gameIcon);
-            console.dir(cross.childNodes);
-            change = false;
-        }
+    // stop if cell already has an icon
+    if (cross.children.length > 0) return;
 
-        else {
+    const gameIcon = document.createElement("i");
 
-            const gameIcon = document.createElement("i");
-            gameIcon.classList.add("fa-solid", "fa-0");
-            cross.appendChild(gameIcon);
-            console.dir(cross.childNodes);
-            change = true;
-        }
 
-})
+    if (change) {
+        gameIcon.classList.add("fa-solid", "fa-x");
+        change = false;
+    } else {
+        gameIcon.classList.add("fa-solid", "fa-circle");
+        change = true;
+    }
+
+    cross.appendChild(gameIcon);
+
+});
