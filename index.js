cceebc7ea3c3b58d6fb1635886472ff380ef9839
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-02-05 17:21:41 +0100

    fixing DOM duplicate of <i> icon

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ let change = true
 
 main.addEventListener("click", (event) => {
     const cross = event.target;
+    const crust = event.currentTarget;
 
-    if(change)
-    {
-        cross.innerHTML = `<i class="fa-solid fa-x"></i>`
-        change = false;
-    }
 
-    else {
-        cross.innerHTML = `<i class="fa-solid fa-0"></i>`
-        change = true;
-    }
+
+
+
+        if(change)
+        {
+            
+            const gameIcon = document.createElement("i");
+            gameIcon.classList.add("fa-solid", "fa-x");
+            cross.appendChild(gameIcon);
+            console.dir(cross.childNodes);
+            change = false;
+        }
+
+        else {
+
+            const gameIcon = document.createElement("i");
+            gameIcon.classList.add("fa-solid", "fa-0");
+            cross.appendChild(gameIcon);
+            console.dir(cross.childNodes);
+            change = true;
+        }
+
 })
