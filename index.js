88c6b87cd8b8d35079a6749ad9af9c8ef179e0cb
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-02-06 10:26:06 +0100

    fetch the position of each choice of x and y

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 const main = document.getElementById("main")
 
-let change = true
+let xChoices = [];
+let oChoices = [];
+let change = true;
+let count = 0;
 
 main.addEventListener("click", (event) => {
     const cross = event.target;
-
-
 
      // make sure only cells are clickable
     if (cross.parentElement !== main) return;
@@ -21,11 +22,23 @@ main.addEventListener("click", (event) => {
     if (change) {
         gameIcon.classList.add("fa-solid", "fa-x");
         change = false;
+        count = count + 1;
+        xChoices.push([...main.children].indexOf(cross));
+        console.log("x chooses:", xChoices);
     } else {
         gameIcon.classList.add("fa-solid", "fa-circle");
         change = true;
+        count = count + 1;
+        oChoices.push([...main.children].indexOf(cross));
+        console.log("o chooses:", oChoices);
     }
 
+
     cross.appendChild(gameIcon);
+
+      if (count === 9) {
+        setTimeout(() => alert("Game over"), 0.5);
+      }
+
 
 });
